b8fcbd3b8e162cfa0e86c53948ea275a193b7bd1
Lorenzo Colelli <lorenzo.colelli@protonmail.com>
2024-04-03 20:30:52 +0200

    generalized save system

---
+++ Assets/Scripts/Managers/SaveManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour {
+
+    public SaveManager Instance { get; private set; }
+    [SerializeField] private string saveFolderPath;
+
+    private void Awake() {
+        //We check if there is already a Singleton of SaveManager
+        if (Instance != null && Instance != this) {
+            Destroy(this);
+            throw new System.Exception($"[{this.name}] >>> An Instance of this Singleton already exists!");
+        } else {
+            //There are not instances
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    public static void SaveData<T>(T dataToSave, string filename) {
+        string data = JsonUtility.ToJson(dataToSave);
+        System.IO.File.WriteAllText(Application.persistentDataPath + $"/{filename}.json", data);
+        Debug.Log(">>> File saved!");
+    }
+
+    public static bool TryReadSavedData<T>(string filename, out T savedData) {
+        if(System.IO.File.Exists(filename)) {
+            // File exists
+            string savedSettings = System.IO.File.ReadAllText(Application.persistentDataPath + $"/{filename}.json");
+            if (savedSettings != "" && savedSettings != null) {
+                // We have collected saved settings
+                savedData = JsonUtility.FromJson<T>(savedSettings);
+                return true;
+            }
+        }
+        // We have no saved settings or file does not exist
+        savedData = default;
+        return false;
+    }
+
+}
+
+[System.Serializable]
+public class SettingsData {
+
+    // Post-processing
+    public bool vignittePP;
+
+    // Key-Bindings
+    public string moveUpKey;
+    public string moveLeftKey;
+    public string moveDownKey;
+    public string moveRightKey;
+    public string interactKey;
+
+    // Audio settings
+    public float masterVolume;
+
+    public SettingsData(bool vignittePP, string moveUpKey, string moveLeftKey, string moveDownKey, string moveRightKey, string interactKey, float masterVolume) {
+        this.vignittePP = vignittePP;
+        this.moveUpKey = moveUpKey;
+        this.moveLeftKey = moveLeftKey;
+        this.moveDownKey = moveDownKey;
+        this.moveRightKey = moveRightKey;
+        this.interactKey = interactKey;
+        this.masterVolume = masterVolume;
+    }
+
+}
--- Assets/Scripts/UI/SettingsUI.cs
+++ Assets/Scripts/UI/SettingsUI.cs
@@ -1,11 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsUI : MonoBehaviour, IMenuUI {
 
+    [Header("UI Elements")]
+    [SerializeField] private TextMeshProUGUI forward;
+    [SerializeField] private TextMeshProUGUI left;
+    [SerializeField] private TextMeshProUGUI back;
+    [SerializeField] private TextMeshProUGUI right;
+    [SerializeField] private TextMeshProUGUI interact;
+    [Space(10)]
+    [SerializeField] private Slider masterVolumeSlider;
+    [Space(10)]
+    [SerializeField] private Toggle vignetteToggle;
+
+    private const string _SETTINGS_FILENAME = "settings";
+    private SettingsData _defaultSettings = new SettingsData(true,"W","A","S","D","E",50.0f);
+    private SettingsData _savedSettings = null;
+
     private void Start() {
+        TryLoadSavedSettings();
         Hide();
+    }
+
+    private void TryLoadSavedSettings() {
+        if(SaveManager.TryReadSavedData<SettingsData>(_SETTINGS_FILENAME, out SettingsData settings)) {
+            _savedSettings = settings;
+        } else {
+            _savedSettings = _defaultSettings;
+        }
+        //Modify keybindings UI
+        forward.text = _savedSettings.moveUpKey;
+        left.text = _savedSettings.moveLeftKey;
+        back.text = _savedSettings.moveDownKey;
+        right.text = _savedSettings.moveRightKey;
+        interact.text = _savedSettings.interactKey;
+
+        //Modify slider UI
+        masterVolumeSlider.value = _savedSettings.masterVolume;
+
+        //Modify checkbox UI
+        vignetteToggle.isOn = _savedSettings.vignittePP;
     }
 
     public void Show() {
@@ -13,7 +51,18 @@ public class SettingsUI : MonoBehaviour, IMenuUI {
     }
 
     public void Hide() {
+        SaveManager.SaveData(GetCurrentUserSettings(), _SETTINGS_FILENAME);
         gameObject.SetActive(false);
+    }
+
+    private SettingsData GetCurrentUserSettings() {
+        if (_savedSettings == null) {
+            _savedSettings = _defaultSettings;
+        } else {
+            // get settings from UI
+
+        }
+        return _savedSettings;
     }
 
 }
